d6576804d4581dad066570fde98f3d6ede6e2929
Tyler Wang <twang.123@live.com>
2014-03-20 01:40:00 -0400

    Finished HW#08

--- 08-LinkedLists/LLDriver.java
+++ 08-LinkedLists/LLDriver.java
@@ -8,8 +8,21 @@ public class LLDriver {
 	//System.out.println(L);
 	L.add("randall");
 	//System.out.println(L);
-	L.add(1,"boo");
+	L.add(0,"boo");
 	System.out.println(L);
+
+	System.out.println(L.get(2));
+
+	System.out.println(L.set(1,"JOHNNY"));
+	System.out.println(L);
+
+	System.out.println(L.remove(2));
+	System.out.println(L);
+
+	System.out.println("Find Sully: "+L.find("Sully"));
+	System.out.println("Find Waldo: "+L.find("Waldo"));
+
+	System.out.println("length: "+L.length());
 
     }
 
--- 08-LinkedLists/MyLinkedList.java
+++ 08-LinkedLists/MyLinkedList.java
@@ -24,19 +24,100 @@ public class MyLinkedList {
 
     public void add (int i, String s){
 	Node temp = new Node(s);
-	if (i==0){
-	    temp.setNext(head);
+	Node index = head;
+	if (i==0)
+	    this.add(s);
+	else{
+	    i--;
+	    while (i>0 && index.getNext() != null){
+		index = index.getNext();
+		i--;
+	    }
+	    temp.setNext(index.getNext());
+	    index.setNext(temp);
+	}
+    }
+
+    public String get(int i){
+	Node temp = head;
+	if (i==0)
+	    return head.getData();
+	else{
+	    while (i>0){
+		if (i>1 && temp.getNext() == null){
+		    return "Exception: out of bounds";
+		}
+		temp = temp.getNext();
+		i--;
+	    }
+	}
+	return temp.getData();
+    }
+
+    public String set(int i,String s){
+	Node temp = new Node(s);
+	Node index = head;
+	if (i ==0){
+	    temp.setNext(head.getNext());
 	    head = temp;
 	}
 	else{
-	    Node n = head;
-	    while (i>0 && n.getNext() != null){
-		n = n.getNext();
+	    while (i>1){
+		index = index.getNext();
+		i--;
 	    }
-	    n.setNext(temp);
+	    temp.setNext(index.getNext().getNext());
+	    index.setNext(temp);
 	}
+	return index.getNext().getData();
+    }
+
+    public String remove(int i){
+	Node index = head;
+	if (i==0){
+	    head = head.getNext();
+	    return index.getData();
+	}
+	else{
+	    while (i>1){
+		index = index.getNext();
+		i--;
+	    }
+	    String s = index.getNext().getData();
+	    index.setNext(index.getNext().getNext());
+	    return s;
+	}
+    }
+
+    public int find(String s){
+	Node index = head;
+	int i = 0;
+	if (index.getData()==s){
+	    return 0;
+	}
+	else{
+	    while (index != null){
 		
+		if (index.getData().equals(s))
+		    return i;
+		else{
+		    i++;
+		    index =  index.getNext();
+		}
+	    }
+	    return -1;
+	}
+    }
+
+    public int length(){
+	Node index = head;
+	int i = 1;
+	while (index.getNext() != null){
+	    i++;
+	    index = index.getNext();
+	}
+	return i;
     }
 	    
-
+	    
 }
